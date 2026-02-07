36f6e1f226bc49da6a8d92fdc72aef31b9fe8c74
David Levy <robedavid@gmail.com>
2026-02-08 00:43:01 +0200

    wip

--- py_src/connect_k_rl/player.py
+++ py_src/connect_k_rl/player.py
@@ -2,6 +2,8 @@ from connect_k_rl.env import ConnectKEnv
 from rl.agent_base import RLAgent
 from rl.env_base import RLEnv
 from rl.random_learner import RandomLearner
+from rl.tabq_learner import TabularQLearner
+from rl.dqn_learner import DQNAgent
 
 
 def play_game(player: RLAgent, game: RLEnv):
@@ -10,24 +12,8 @@ def play_game(player: RLAgent, game: RLEnv):
     game.render()
 
 
-def train_tabular(**kwargs):
-    from rl.tabq_learner import TabularQLearner
-
-    game = ConnectKEnv(**kwargs)
+if __name__ == "__main__":
+    game = ConnectKEnv(nrows=9, ncols=8, k=4, use_gravity=True)
     player = TabularQLearner()
     player.train(game=game, eval_every=5_000)
-    return player, game
-
-
-def train_nn(**kwargs):
-    from rl.dqn_learner import DQNAgent
-
-    game = ConnectKEnv(**kwargs)
-    player = DQNAgent(n_actions=game.nrows * game.ncols)
-    player.train(game=game, eval_every=5_000)
-    return player, game
-
-
-if __name__ == "__main__":
-    player, game = train_nn()
     play_game(player, game)
