e17acd68777be95db57f17c7a0a4fd113c5846ad
David Levy <robedavid@gmail.com>
2026-02-08 01:00:58 +0200

    wip

--- py_src/main.py
+++ py_src/main.py
@@ -1,7 +1,12 @@
-import sys, os
+from connect_k_rl.env import ConnectKEnv
+from rl.agent_base import RLAgent
+from rl.env_base import RLEnv
+from rl.random_learner import RandomLearner
+from rl.tabq_learner import TabularQLearner
+from rl.dqn_learner import DQNAgent
 
 if __name__ == "__main__":
-    print("Hello World!")
-    print("cwd:", os.getcwd())
-    print("first paths: ", sys.path)
-    print(os.environ.get("PYTHONPATH"))
+    game = ConnectKEnv(nrows=9, ncols=8, k=4, use_gravity=True)
+    player = DQNAgent(n_actions=8)
+    player.train(game=game, eval_every=5_000, episodes=30_000)
+    RLAgent.agent_duel(player, RandomLearner(), game=game, verbose=True)
